37120395c8561ba9dc2f823088487c8e58573ab3
Sumita <ssumita_be22@thapar.edu>
2025-07-23 02:18:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,9 @@ def predict():
     except Exception as e:
         return f"❌ Error: {str(e)}"
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
